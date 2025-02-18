ea852389412c5b2e35f3d081499d5201534dc44f
Jason Right <right.jason21@gmail.com>
2025-02-18 15:28:09 +0530

    added homecontroller message

---
+++ src/main/java/com/healthcare/pulsemonitor/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.healthcare.pulsemonitor.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "Heart Rate Monitor API is running. Check API docs for available endpoints.";
+    }
+}
